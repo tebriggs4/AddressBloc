13aee1ab84c4594e98b80396d1568baba197f90b
Tara Briggs <tebriggs4@gmail.com>
2017-03-18 15:24:03 +0000

    assignment-6-menu complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -12,10 +12,11 @@ class MenuController
         # Display the main menu options to the command line. 
         puts "Main Menu - #{address_book.entries.count} entries"
         puts "1 - View all entries"
-        puts "2 - Create an entry"
-        puts "3 - Search for an entry"
-        puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
+        puts "2 - View Entry Number n"
+        puts "3 - Create an entry"
+        puts "4 - Search for an entry"
+        puts "5 - Import entries from a CSV"
+        puts "6 - Exit"
         print "Enter your selection: "
  
         # Retrieve user input from the command line using gets. gets reads the next line from standard input.
@@ -29,17 +30,21 @@ class MenuController
                 main_menu
             when 2
                 system "clear"
-                create_entry
+                view_one_entry
                 main_menu
             when 3
                 system "clear"
-                search_entries
+                create_entry
                 main_menu
             when 4
                 system "clear"
-                read_csv
+                search_entries
                 main_menu
             when 5
+                system "clear"
+                read_csv
+                main_menu
+            when 6
                 puts "Good-bye!"
                 # Terminate the program using exit(0). 0 signals the program is exiting without an error. 
                 exit(0)
@@ -64,6 +69,23 @@ class MenuController
  
         system "clear"
         puts "End of entries"
+    end
+    
+    def view_one_entry
+        print "Entry number: "
+        index = gets.chomp.to_i
+        if (address_book.entries.count == 0)
+            puts "Address Book is empty"
+        else
+            if (index >= address_book.entries.count)
+                puts "#{index} is not a valid input"
+                view_one_entry
+            else
+                puts @address_book.entries[index]
+                main_menu
+            end
+        end
+        
     end
  
     def create_entry
