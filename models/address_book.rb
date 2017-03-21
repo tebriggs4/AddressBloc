080158d7b5692baa6c78ed8e66e31dd0f9a9e52b
Tara Briggs <tebriggs4@gmail.com>
2017-03-21 23:17:20 +0000

    checkpoint-8-searching complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -30,6 +30,7 @@ class AddressBook
         entries.each do |entry|
             if (name == entry.name && phone_number == entry.phone_number && email == entry.email)
                 entry_to_delete = entry
+                break
             end
             
         end
@@ -51,6 +52,36 @@ class AddressBook
             row_hash = row.to_hash
             add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
         end
+    end
+    
+    # Search AddressBook for a specific entry by name
+    def binary_search(name)
+        # We save the index of the leftmost item in the array in a variable named lower, 
+        # and the index of rightmost item in the array in upper. 
+        lower = 0
+        upper = entries.length - 1
+ 
+        # we loop while our lower index is less than or equal to our upper index.
+        while lower <= upper
+            # We find the middle index by taking the sum of lower and upper and dividing it by two.
+            # Ruby will truncate any decimal numbers, so if upper is five and lower is zero then mid will
+            # get set to two. Then we retrieve the name of the entry at the middle index and store it in mid_name.
+            mid = (lower + upper) / 2
+            mid_name = entries[mid].name
+ 
+            # We compare the name that we are searching for, name, to the name of the middle index, mid_name. We
+            # use the == operator when comparing the names which makes the search case sensitive.
+            if name == mid_name
+                return entries[mid]
+            elsif name < mid_name
+                upper = mid - 1
+            elsif name > mid_name
+                lower = mid + 1
+            end
+        end
+ 
+        # If we divide and conquer to the point where no match is found, we return nil.
+        return nil
     end
    
 end
